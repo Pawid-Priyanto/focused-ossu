a079ab5bd61123151abff54dc5cd30616f641c6b
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-15 21:48:44 +0700

    exercise 1 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
-import "./App.css";
-import React from "react";
+const Headers = (props) => {
+  const { course } = props;
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part, idx) => (
+        <Part part={part.part} key={part.part} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ total }) => {
+  return <p>Number of exercises {total}</p>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      {part}, {exercises}
+    </div>
+  );
+};
 
 const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  // return (
-  //   <div>
-  //     <p>Hello world, it is {now.toString()}</p>
-  //     {a} plus {b} is {a + b}{" "}
-  //   </div>
-  // );
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("p", null, "Hello World ", now.toDateString()),
-    React.createElement("p", null, a, " plus ", b, " is ", a + b)
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const parts = [
+    { part: "Fundamentals of React", exercises: 10 },
+    { part: "Using props to pass data", exercises: 7 },
+    { part: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Headers course={course} />
+      <Content parts={parts} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+      {/* <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+    </div>
   );
 };
 
