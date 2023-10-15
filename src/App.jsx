0e032466378d1f9c6954cada3a1cb727877b6f8e
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-15 21:55:29 +0700

    exercise 1 1.1 v2 update total reduce

--- src/App.jsx
+++ src/App.jsx
@@ -38,12 +38,13 @@ const App = () => {
     { part: "Using props to pass data", exercises: 7 },
     { part: "State of a component", exercises: 14 },
   ];
+  const totalReduce = parts.reduce((total, part) => total + part.exercises, 0);
 
   return (
     <div>
       <Headers course={course} />
       <Content parts={parts} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={totalReduce} />
       {/* <h1>{course}</h1>
       <p>
         {part1} {exercises1}
