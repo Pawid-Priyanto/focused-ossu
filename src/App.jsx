bc4bb979c62075a27808b227b976e87c97846c52
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-19 11:30:57 +0700

    ossu 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -5,57 +5,50 @@ const Headers = (props) => {
 
 const Content = ({ parts }) => {
   return (
-    <>
-      {parts.map((part, idx) => (
-        <Part part={part.part} key={part.part} exercises={part.exercises} />
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
       ))}
-    </>
+    </div>
   );
 };
 
-const Total = ({ total }) => {
+const Total = ({ parts }) => {
+  const total = parts.reduce((total, part) => total + part.exercises, 0);
   return <p>Number of exercises {total}</p>;
 };
 
-const Part = ({ part, exercises }) => {
+const Part = ({ part }) => {
   return (
     <div>
-      {part}, {exercises}
+      {part.name}, {part.exercises}
     </div>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const parts = [
-    { part: "Fundamentals of React", exercises: 10 },
-    { part: "Using props to pass data", exercises: 7 },
-    { part: "State of a component", exercises: 14 },
-  ];
-  const totalReduce = parts.reduce((total, part) => total + part.exercises, 0);
+
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Headers course={course} />
       <Content parts={parts} />
-      <Total total={totalReduce} />
-      {/* <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Total parts={parts} />
     </div>
   );
 };
