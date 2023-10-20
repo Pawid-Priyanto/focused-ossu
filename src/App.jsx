267dec99fe03910ad9a4d5f6f2fa3f40abddaf41
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 21:35:26 +0700

    complex-state final

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,12 @@ function App() {
   });
 
   const handleLeft = () => {
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right,
-    };
-    setClicks(newClicks);
+    setClicks({ ...clicks, left: clicks.left + 1 });
   };
   const handleRight = () => {
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1,
-    };
-    setClicks(newClicks);
+    setClicks({ ...clicks, right: clicks.right + 1 });
   };
+
   return (
     <div>
       {clicks.left}
