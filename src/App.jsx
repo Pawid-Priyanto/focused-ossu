f33783bb3c9f75357ff88fe9421d47971e8fa576
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 21:18:10 +0700

    pass props to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from "react";
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const handleIncrease = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
+  console.log("rendering with counter value", counter);
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
 
   return (
-    <>
-      <div>{counter}</div>
-      <button onClick={handleIncrease}>plus</button>
-      <button onClick={setToZero}>zero</button>
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
   );
 };
 
