744ed7ba80d45679f3bac92d2e747dd7de5f7ac9
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 23:02:19 +0700

    function return function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
 function App() {
   const [value, setValue] = useState(0);
-  const setToValue = (val) => () => {
-    setValue(val);
+  const setToValue = (newValue) => () => {
+    setValue(newValue);
   };
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>zero</button>
-      <button onClick={setToValue(value + 1)}>plus 1</button>
+      <Button handleClick={() => setValue(1000)} text="thousand" />
+      <Button handleClick={() => setValue(0)} text="set to zero" />
+      <Button handleClick={() => setValue(value + 1)} text={"plus 1"} />
     </div>
   );
 }
