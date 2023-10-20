5d45dccbc9170be562302dd8c6dc5b97750f6b58
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 22:47:34 +0700

    event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
 
-const App = () => {
-  // these are ok
-  const [age, setAge] = useState(0);
-  const [name, setName] = useState("Juha Tauriainen");
-
-  if (age > 10) {
-    // this does not work!
-    const [foobar, setFoobar] = useState(null);
-  }
-
-  for (let i = 0; i < age; i++) {
-    // also this is not good
-    const [rightWay, setRightWay] = useState(false);
-  }
-
-  const notGood = () => {
-    // and this is also illegal
-    const [x, setX] = useState(-1000);
-  };
-
+function App() {
+  const [value, setValue] = useState(0);
   return (
-    //...
     <div>
-      Rules Of Hooks
-      <p>
-        my name {name}, i am {age} years old
-      </p>
+      {value}
+      <button onClick={() => setValue(0)}>reset to zero</button>
     </div>
   );
-};
+}
 
 export default App;
