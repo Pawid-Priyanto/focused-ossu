6bee4fc8a315f03ae73a3eff7882779f8714e780
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 21:30:35 +0700

    complex-state v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+function App() {
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  console.log("rendering with counter value", counter);
-
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
+  const handleLeft = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right,
+    };
+    setClicks(newClicks);
   };
-
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
+  const handleRight = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks);
   };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
-
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeft}>left</button>
+      <button onClick={handleRight}>right</button>
+      {clicks.right}
     </div>
   );
-};
+}
 
 export default App;
