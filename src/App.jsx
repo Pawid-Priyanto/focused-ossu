21c524061771d213e92677060b36dfe791befdac
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-20 21:00:18 +0700

    page re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
-import React from "react";
-
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
-  return (
-    <>
-      <h3>
-        Hello, I am {props.name} {props.age} years old
-      </h3>
-      <h2>I am propbably born on {bornYear()}</h2>
-    </>
-  );
+const App = (props) => {
+  const { counter } = props;
+  return <div>{counter}</div>;
 };
-
-function App() {
-  const name = "Peter";
-  const age = 43;
-  return (
-    <div>
-      <h1>Grettings</h1>
-      <Hello name="John" age={30} />
-      {/* <Hello name={name} age={age} /> */}
-    </div>
-  );
-}
 
 export default App;
