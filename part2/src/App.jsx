c7224e171b1dc623f0e3c2ecee879e9ea0ed229f
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-22 18:09:57 +0700

    nitial state part2

---
+++ part2/src/App.jsx
@@ -0,0 +1,20 @@
+import Note from "./components/Note";
+
+import React from "react";
+
+const App = (props) => {
+  const { notes } = props;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.content} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
