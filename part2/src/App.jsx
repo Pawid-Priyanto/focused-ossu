1c0a17bf60d0d3737fc8f746d5704df488231217
pawitpriyanto <Pawit.Priyanto@sera.astra.co.id>
2023-10-29 01:16:40 +0700

    sendin data from server

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import noteService from "./services/apiUrl";
 const App = (props) => {
   const [newNote, setNewNote] = useState("");
   const [note, setNote] = useState(props.notes);
@@ -15,8 +16,25 @@ const App = (props) => {
       id: note.length + 1,
     };
 
-    setNote(note.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNote(note.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const noted = note.find((n) => n.id === id);
+    const changedNote = { ...noted, important: !noted.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNote(note.map((n) => (n.id !== id ? n : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note ${note.content} was already delted from server`);
+        setNote(note.filter((n) => n.id !== id));
+      });
   };
 
   const handleChangeNote = (e) => {
@@ -24,6 +42,11 @@ const App = (props) => {
     console.log("=================");
     setNewNote(e.target.value);
   };
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => setNote(initialNotes));
+  }, []);
+
   return (
     <div>
       <h1>Notes</h1>
@@ -34,7 +57,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNotes}>
